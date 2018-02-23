27854137fe9f63766ad1e83dd5812c517ca16025
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-02-23 10:43:53 -0300

    02. Isolando código repetido em serviços

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { FotoComponent } from "../foto/foto.component";
-import { Http, Headers } from '@angular/http';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FotoService } from "../foto/foto.service";
 
 @Component({
     moduleId: module.id,
@@ -11,12 +11,12 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class CadastroComponent { 
 
     foto: FotoComponent = new FotoComponent();
-    http: Http;
+    service: FotoService;
     meuForm: FormGroup;
 
-    constructor(http: Http, fb: FormBuilder) {
+    constructor(service: FotoService, fb: FormBuilder) {
 
-        this.http = http;
+        this.service = service;
 
         this.meuForm = fb.group({
 
@@ -35,7 +35,7 @@ export class CadastroComponent {
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
 
-        this.http.post('v1/fotos', JSON.stringify(this.foto), {headers: headers})
+        this.service.cadastra(this.foto)
             .subscribe(() => {
                 this.foto = new FotoComponent();
                 console.log('Foto salva com sucesso');
