58b77b79d9b053b622965e1873c5c0fce24a8b1a
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-02-26 11:51:05 -0300

    atualizando a tela na remoção de uma foto

--- alurapic/client/app/listagem/listagem.component.ts
+++ alurapic/client/app/listagem/listagem.component.ts
@@ -11,10 +11,11 @@ import { FotoService } from "../foto/foto.service";
 export class ListagemComponent {
 
     fotos: FotoComponent[] = [];
+    service: FotoService;
 
     constructor(service: FotoService) {
-
-        service.lista()
+        this.service = service;
+        this.service.lista()
             .subscribe(
                 fotos => this.fotos = fotos,
                 erro => console.log(erro)
@@ -23,6 +24,17 @@ export class ListagemComponent {
 
     remove(foto: FotoComponent): void {
 
-        console.log(foto.titulo);
+        this.service
+            .remove(foto)
+            .subscribe(
+                fotos =>  {
+                    console.log('Foto removida com sucesso')
+                    let novasFotos = this.fotos.slice(0);
+                    let indice = novasFotos.indexOf(foto);
+                    novasFotos.splice(indice, 1);
+                    this.fotos = novasFotos;
+                },
+                erro => console.log(erro)
+            )
     }
 }
