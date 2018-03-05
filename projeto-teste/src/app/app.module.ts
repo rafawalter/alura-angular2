1e231d2e7a480ad7c672178d175ffaf0009ff5be
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2018-03-05 13:42:57 -0300

    criando componentes e modulos

--- projeto-teste/src/app/app.module.ts
+++ projeto-teste/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TabelaComponent } from './tabela/tabela.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaComponent
   ],
   imports: [
     BrowserModule
